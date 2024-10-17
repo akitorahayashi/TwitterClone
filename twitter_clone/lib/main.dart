158ab0ca96844f1227c0dc1bcbae19f3b18481a5
林明虎 <hayashi20030703@gmail.com>
2024-10-17 21:10:33 +0900

    river_podを導入し、provider_scopeを設定した

--- twitter_clone/lib/main.dart
+++ twitter_clone/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import './app.dart';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 void main() {
-  runApp(const TwitterClone());
+  runApp(const ProviderScope(
+    child: TwitterClone(),
+  ));
 }
